0f6817180081c1ecb76e138f0972aca15d2babbe
Mehmet <mehmetkusken@gmail.com>
2021-08-09 15:51:18 -0600

    commit tree e08ece5a3b6aa65e298fd4257d20b8eb9fbcd66c

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+// 2
+let random = Math.ceil(Math.random());
+
+// 3 
+const num3 = 100;
+const num4 = 6;
+const mod = num3 % num4;
+
+// 4
+let max = Math.max(1, 20)
